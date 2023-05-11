e2168bcc9770d2b0a3c3cffe31b3f62cddf0eb3a
Clinton Brito <clintoncsb@gmail.com>
2023-05-11 12:22:38 -0300

    :white_check_mark: [Req. 5] Pokedex - testes ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -13,32 +13,48 @@ describe('Teste o componente <Pokedex.js />', () => {
     });
     expect(titleFound).toBeInTheDocument();
   });
-  test('é exibido o próximo Pokémon da lista quando o botão `Próximo Pokémon` é clicado', () => {
+  test('é exibido o próximo Pokémon da lista quando o botão `Próximo Pokémon` é clicado & a Pokédex contém um botão para resetar o filtro', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
     const allButton = screen.getByRole('button', { name: /All/i });
     userEvent.click(allButton);
     const pikachu = screen.getByText(/pikachu/i);
     expect(pikachu).toBeInTheDocument();
+    expect(allButton).not.toHaveProperty('pokemon-type-button');
 
     const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(nextPokemonButton).toBeInTheDocument();
     userEvent.click(nextPokemonButton);
-
     const charmander = screen.getByText(/charmander/i);
     expect(charmander).toBeInTheDocument();
 
+    userEvent.click(allButton);
+    expect(pikachu).toBeInTheDocument();
+
+    userEvent.click(nextPokemonButton);
+    expect(charmander).toBeInTheDocument();
+
+    userEvent.click(nextPokemonButton);
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
+  });
+
+  test('é mostrado apenas um Pokémon por vez e a Pokédex tem os botões de filtro', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
     const typeButtons = screen.getAllByTestId('pokemon-type-button');
+    const allButton = screen.getByRole('button', { name: /All/i });
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+
     typeButtons.forEach((button) => {
       if (button.textContent === 'Psychic') {
         userEvent.click(button);
         expect(allButton).toBeInTheDocument();
       }
     });
+
     const alakazam = screen.getByText(/alakazam/i);
     expect(alakazam).toBeInTheDocument();
-    userEvent.click(allButton);
-    expect(pikachu).toBeInTheDocument();
     userEvent.click(nextPokemonButton);
-    expect(charmander).toBeInTheDocument();
+    const mew = screen.getByText(/mew/i);
+    expect(mew).toBeInTheDocument();
   });
 });
